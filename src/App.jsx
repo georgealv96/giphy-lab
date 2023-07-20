86e8523a1d711265169ec840ddea0a9911e0924d
georgealv96 <george1996alvarado@gmail.com>
2023-07-20 13:46:19 -0600

    searching for something new in the api

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import SearchForGif from './SearchForGif/SearchForGif'
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   // setting a state for whatever the user searches
-  const [searchTerm, setSearchTerm] = useState('')
+  const [searchTerm, setSearchTerm] = useState('hello')
+
+  useEffect(() => {
+    console.log('effect running')
+    async function getGifInfo() {
+      const gifUrl = `https://api.giphy.com/v1/gifs/search?api_key=iz3kuyHma6E5D1IpGrZyflhntS2cLTLH&q=${searchTerm}`
+      try {
+        const apiResponse = await fetch(gifUrl)
+        const data = await apiResponse.json()
+        console.log('data > ', data)
+      } catch (err) {
+        console.log(err, ' error from api cal')
+      }
+    }
+
+    getGifInfo()
+  }, [searchTerm])
 
   function getGif(searchedGif) {
     setSearchTerm(searchedGif)
